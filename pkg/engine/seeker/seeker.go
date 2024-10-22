6366e93fe51f228af22cb3770bed167262b2ff8f
just-hms <alessandroversari1@gmail.com>
2024-10-22 19:37:57 +0200

    edit: preload seeker

--- pkg/engine/seeker/seeker.go
+++ pkg/engine/seeker/seeker.go
@@ -3,6 +3,7 @@ package seeker
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/just-hms/pulse/pkg/spimi/inverseindex"
@@ -17,45 +18,66 @@ type Seeker struct {
 	start, cur, end uint32
 
 	Term withkey.WithKey[inverseindex.LocalTerm]
+
+	// Cache for faster processing
+	postingBuffer []byte
+	freqBuffer    []byte
 }
 
 func NewSeeker(postingFile, freqFile *os.File, t withkey.WithKey[inverseindex.LocalTerm]) *Seeker {
-	return &Seeker{
-		Term:  t,
-		start: t.Value.StartOffset,
-		cur:   t.Value.StartOffset,
-		end:   t.Value.EndOffset,
+	// Define a reasonable buffer size (adjust as needed)
+	bufferSize := int(t.Value.EndOffset) - int(t.Value.StartOffset)
 
-		postingFile: postingFile,
-		freqFile:    freqFile,
+	s := &Seeker{
+		Term:          t,
+		start:         t.Value.StartOffset,
+		cur:           0,
+		end:           t.Value.EndOffset,
+		postingFile:   postingFile,
+		freqFile:      freqFile,
+		postingBuffer: make([]byte, bufferSize),
+		freqBuffer:    make([]byte, bufferSize),
 	}
+	s.fill()
+	return s
 }
 
-func (s *Seeker) Next() {
-	if _, err := s.postingFile.Seek(int64(s.cur), 0); err != nil {
-		panic(fmt.Sprintf("seeker: %v, while reading docs, with error: %v", s, err))
-	}
-	if err := binary.Read(s.postingFile, binary.LittleEndian, &s.DocumentID); err != nil {
-		panic(fmt.Sprintf("seeker: %v, while reading docs, with error: %v", s, err))
-	}
-
-	if _, err := s.freqFile.Seek(int64(s.cur), 0); err != nil {
-		panic(fmt.Sprintf("seeker: %v, while reading docs, with error: %v", s, err))
+// Read next chunk of data into buffer
+func (s *Seeker) fill() error {
+	var err error
+	if _, err = s.postingFile.Seek(int64(s.cur), io.SeekStart); err != nil {
+		return fmt.Errorf("error seeking postingFile: %v", err)
 	}
 
-	if err := binary.Read(s.freqFile, binary.LittleEndian, &s.Frequence); err != nil {
-		panic(fmt.Sprintf("seeker: %v, while reading freqs, with error: %v", s, err))
+	_, err = s.postingFile.Read(s.postingBuffer)
+	if err != nil && err != io.EOF {
+		return fmt.Errorf("error reading postingFile: %v", err)
 	}
 
-	// todo: specify bit position when using compression
+	if _, err = s.freqFile.Seek(int64(s.cur), io.SeekStart); err != nil {
+		return fmt.Errorf("error seeking freqFile: %v", err)
+	}
+
+	_, err = s.freqFile.Read(s.freqBuffer)
+	if err != nil && err != io.EOF {
+		return fmt.Errorf("error reading freqFile: %v", err)
+	}
+
+	return nil
+}
+
+func (s *Seeker) Next() error {
+	s.DocumentID = binary.LittleEndian.Uint32(s.postingBuffer[s.cur : s.cur+4])
+	s.Frequence = binary.LittleEndian.Uint32(s.freqBuffer[s.cur : s.cur+4])
 	s.cur += 4
+	return nil
 }
 
 func (s *Seeker) String() string {
 	return fmt.Sprintf("{%s %d:%d:%d}", s.Term.Key, s.start, s.cur, s.end)
 }
 
-// EOD (end of docs) returns true if the seeker have reached the end of the term's document
+// EOD (end of docs) returns true if the seeker has reached the end of the term's document
 func EOD(s *Seeker) bool {
-	return s.cur > s.end
+	return s.start+s.cur >= s.end
 }
