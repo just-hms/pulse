f9b9dfb6cf5f7523214b5dc79415aaf6e7e09b2a
just-hms <alessandroversari1@gmail.com>
2025-01-16 16:39:10 +0100

    edit: seeker structure

--- pkg/engine/seeker/seeker.go
+++ pkg/engine/seeker/seeker.go
@@ -13,24 +13,21 @@ import (
 type Seeker struct {
 	postings, freqs io.ReaderAt
 
-	buf [4]byte
+	curOffset uint32
+	buf       [4]byte
 
 	DocumentID    uint32
 	TermFrequency uint32
-
-	start, cur, end uint32
 
 	Term withkey.WithKey[inverseindex.LocalTerm]
 }
 
 func NewSeeker(postings, freqs io.ReaderAt, t withkey.WithKey[inverseindex.LocalTerm]) *Seeker {
 	s := &Seeker{
-		Term:     t,
-		start:    t.Value.StartOffset,
-		cur:      0,
-		end:      t.Value.EndOffset,
-		postings: postings,
-		freqs:    freqs,
+		Term:      t,
+		curOffset: 0,
+		postings:  postings,
+		freqs:     freqs,
 	}
 	return s
 }
@@ -40,26 +37,26 @@ func (s *Seeker) Next() error {
 		return errors.New("cannot read further")
 	}
 
-	pos := s.start + s.cur
+	ptr := s.Term.Value.StartOffset + s.curOffset
 
-	if _, err := s.postings.ReadAt(s.buf[:], int64(pos)); err != nil {
+	if _, err := s.postings.ReadAt(s.buf[:], int64(ptr)); err != nil {
 		return fmt.Errorf("error reading postingFile: %v", err)
 	}
 	s.DocumentID = binary.LittleEndian.Uint32(s.buf[:])
 
-	if _, err := s.freqs.ReadAt(s.buf[:], int64(pos)); err != nil {
+	if _, err := s.freqs.ReadAt(s.buf[:], int64(ptr)); err != nil {
 		return fmt.Errorf("error reading freqFile: %v", err)
 	}
 	s.TermFrequency = binary.LittleEndian.Uint32(s.buf[:])
-	s.cur += 4
+	s.curOffset += 4
 	return nil
 }
 
 func (s *Seeker) String() string {
-	return fmt.Sprintf("{%s %d:%d:%d}", s.Term.Key, s.start, s.cur, s.end)
+	return fmt.Sprintf("{%s %d:%d:%d}", s.Term.Key, s.Term.Value.StartOffset, s.curOffset, s.Term.Value.EndOffset)
 }
 
 // EOD (end of docs) returns true if the seeker has reached the end of the term's document
 func EOD(s *Seeker) bool {
-	return s.start+s.cur >= s.end
+	return s.Term.Value.StartOffset+s.curOffset >= s.Term.Value.EndOffset
 }
